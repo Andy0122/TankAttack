02c5d28c51031aa76359fdbfaa4e947f593f20e8
Dylan Elizondo <dyelizondo@estudiantec.cr>
2024-09-29 16:33:21 -0600

    .

---
+++ include/Model.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "systems/GridGraph.h"
+
+
+/**
+ * @brief Model class
+ */
+class Model {
+public:
+    /**
+    * @brief Model Constructor
+    */
+    Model();
+
+    /**
+    *  @brief Gets the map of the game
+    *  @returns GridGraph* Map of the game
+    */
+    [[nodiscard]] GridGraph* getMap() const;
+
+private:
+    GridGraph* map; ///< Map of the game
+};
